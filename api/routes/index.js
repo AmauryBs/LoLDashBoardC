93bf27106a136849dca77d797f0a0ea7d292d834
Amaury <43411870+AmauryBs@users.noreply.github.com>
2020-11-06 09:41:13 +0100

    bug fix

--- api/routes/index.js
+++ api/routes/index.js
@@ -11,14 +11,11 @@ router.get('/update', (req, res) => {
 
 router.get('/loadGame', (req, res) => {
     controller.loadGame(req, res);
+});
+
 router.post('/gameHistory', (req, res) => {
     controller.historyInfo(req, res);
 });
-
-/*router.get('/getChampionName', (req, res) => {
-    controller.ChampionIdToName(req.id, res);
-    console.log(req);
-});*/
 
 router.get('/winrateChamp', (req, res) => {
     controller.winrateChamp(req, res);
